aebb94d7517ba943edce4dc66b89685440856185
FuzzilFox1424 <chinh9675pl365@gmail.com>
2024-12-04 13:36:40 +0700

    w4 for RDBMS

---
+++ 4.RDBMS/Coffee_shop/requirement.py
@@ -0,0 +1,12 @@
+# In this project, you will be working with a subset of data from the Coffee shop sample data.
+
+# You will use a modified version of the data for the project, so to succeed in the project, download the linked files when prompted in the instructions. You do not need to use any data from the source.
+
+# In your scenario, you will be working with data from the following sources:
+
+# Staff information held in a spreadsheet at headquarters (HQ)
+# Sales outlet information held in a spreadsheet at HQ
+# Sales data output as a CSV file from the POS system in the sales outlets
+# Customer data output as a CSV file from a custom customer relationship management system
+# Product information maintained in a spreadsheet exported from your supplier's database
+
---
+++ USACO/pair.py
@@ -0,0 +1,13 @@
+"""
+Output:
+(5, 'asdf')
+5
+True
+"""
+
+p1 = (5, "asdf")
+print(p1)
+print(p1[0])  # access the first element of the tuple
+
+p2 = (5, "asdfd")
+print(p1 < p2)
